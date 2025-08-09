bf41b46a4af406a7d87b4087f3627feeeae9a2f7
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-08-09 08:36:50 -0300

    ec.h

---
+++ src/ec.h
@@ -0,0 +1,30 @@
+#ifndef EC_CUDA_H
+#define EC_CUDA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    unsigned int x[8];
+    unsigned int y[8];
+    int infinity;
+} ECPoint;
+
+void point_init(ECPoint *point);
+
+void point_add(ECPoint *R, const ECPoint *P, const ECPoint *Q);
+
+void point_double(ECPoint *R, const ECPoint *P);
+
+void scalar_mult(ECPoint *R, const char *k_hex, const ECPoint *P);
+
+int point_is_valid(const ECPoint *point);
+
+void get_compressed_public_key(unsigned char *out, const ECPoint *public_key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EC_CUDA_H */
