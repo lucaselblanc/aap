6bd707b9ce8ad7c8422f75d57579d28a01b8bb55
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-08-09 08:45:29 -0300

    ec.h

--- src/ec.h
+++ src/ec.h
@@ -12,15 +12,11 @@ typedef struct {
 } ECPoint;
 
 void point_init(ECPoint *point);
-
+void point_clear(ECPoint *point);
 void point_add(ECPoint *R, const ECPoint *P, const ECPoint *Q);
-
 void point_double(ECPoint *R, const ECPoint *P);
-
 void scalar_mult(ECPoint *R, const char *k_hex, const ECPoint *P);
-
 int point_is_valid(const ECPoint *point);
-
 void get_compressed_public_key(unsigned char *out, const ECPoint *public_key);
 
 #ifdef __cplusplus
